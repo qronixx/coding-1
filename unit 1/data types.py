340a080791eefcefd13e7d5656cf7f4c195697a8
qronixx <tthompson-short284@boyslatin.net>
2025-08-22 12:44:34 +0000

    first commit

---
+++ unit 1/data types.py
@@ -0,0 +1,21 @@
+"this is a string"
+
+"Tyheem Thompson-Short"
+
+"20081101"
+
+"this is also a string"
+
+"@#$@#$@#$@#$"
+
+# this is a comment. It is a message that only humans can see.
+# It is visible to the computer.
+
+# Intergers
+1010
+2020
+3030
+4040
+5050
+6060
+7070
