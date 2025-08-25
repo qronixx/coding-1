513496b662f657085ac06aa3fae643ca399ed06e
qronixx <tthompson-short284@boyslatin.net>
2025-08-25 12:33:54 +0000

    Class work

--- unit 1/data types.py
+++ unit 1/data types.py
@@ -1,4 +1,4 @@
-# Strings
+# Strings - characters wrapped in quotations.
 
 "this is a string"
 
@@ -13,7 +13,7 @@
 # this is a comment. It is a message that only humans can see.
 # It is visible to the computer.
 
-# Intergers
+# Intergers - number values that can only be whole numbers.
 1010
 2020
 3030
@@ -21,3 +21,20 @@
 5050
 6060
 7070
+
+# Floats- number values that can only be decimal numbers.
+10.10
+23.38924
+0.001
+
+# Boolean - is a value that can be either true or false.
+True
+False
+0 - False
+1 - True
+
+# Variables - containers for data
+
+schoolName = 'Boys Latin'
+adress = '123 main street'
+phone = xxx-xxx-xxxx
