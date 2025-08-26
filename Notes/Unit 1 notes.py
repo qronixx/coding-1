fc838030c2d9a85420bfad6b6d4119c424023801
qronixx <tthompson-short284@boyslatin.net>
2025-08-26 12:44:56 +0000

    ntoes

---
+++ Notes/Unit 1 notes.py
@@ -0,0 +1,11 @@
+# 8/26/25
+
+# Syntax The rules of how a prgramming language is written
+
+# Github - A platform for writing, maaging, and sharing codes
+
+# Codespaces - The section of github where users can write and run code
+
+# Repository - The section of github where users can share and view code in read only mode
+
+# 
---
+++ unit 1/operators.py
@@ -0,0 +1,7 @@
+# Operators
+# perators are a construct (built-in system) that give datat types more actions anad power.
+
+# Arithmetic family of operators (math)
+# Arithmetic operators are just math operations
+
+print(2+2)
