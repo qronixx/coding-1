e42e775f631a17a5b1cba013d78d632686f63562
qronixx <tthompson-short284@boyslatin.net>
2025-09-02 12:47:21 +0000

    9/2/2025

---
+++ unit 1/BuiltInFunction.py
@@ -0,0 +1,7 @@
+# built in functions - pre-written code tha is stored in the programminglanugae. So long as you knpw the keyword you can use its power.
+
+# float()
+# print()
+# input()
+
+#str() - allows us to change data into strings. it wraps quotes around the data
