26081d1db4bf5e7d6ed9c011d086e06f7da783ec
qronixx <tthompson-short284@boyslatin.net>
2025-09-03 12:47:42 +0000

    9/3/2025

---
+++ Notes/CastomgAvticity.py
@@ -0,0 +1,6 @@
+# Problem 1
+
+Name = input('What is your name: ')
+Age = input('Wjat is your age? ')
+
+print('Hello my name is' + str(name) + 'I am' + int(Age) + 'years old')
--- unit 1/BuiltInFunction.py
+++ unit 1/BuiltInFunction.py
@@ -1,7 +1,13 @@
 # built in functions - pre-written code tha is stored in the programminglanugae. So long as you knpw the keyword you can use its power.
 
-# float()
-# print()
-# input()
+print('tyheem')
 
-#str() - allows us to change data into strings. it wraps quotes around the data
+# input() function - this is a vuilt in function that allows users to type in data into a program and assign it to a variable. 
+# The newly inputted data will ALWAYS be treated as a string
+
+# when we combine 2 or more strings together with the plus sign. It is called string conactentation
+
+number = input('give me a number to calculate: ')
+
+print (3 * float(number))
+
