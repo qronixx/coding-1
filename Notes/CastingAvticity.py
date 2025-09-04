231d33d957fc090b3219738502f51f06b6cc2b65
qronixx <tthompson-short284@boyslatin.net>
2025-09-04 12:44:08 +0000

    09/04/2025

---
+++ Notes/CastingAvticity.py
@@ -0,0 +1,16 @@
+# Problem 1
+
+Name = input('What is your name: ')
+Age = input('What is your age? ')
+
+print('Hello my name is ' + Name + ' I am ' + str(Age) + ' years old')
+
+# problem 2
+value = 'true'
+
+print(True == bool(value)) # result should benbe true
+
+# problem 3
+bonus = '200'
+
+print(300 + int(bonus))
