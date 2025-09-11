ae7771cb3b4498911976a4a173933290250e7646
qronixx <tthompson-short284@boyslatin.net>
2025-09-11 12:48:57 +0000

    9/11/25

---
+++ unit 2/functions.py
@@ -0,0 +1,27 @@
+# Functions - instructions for executing code.
+
+# to create a finctopn, we starrt with the "def" keyboard.
+# this is the finction definition
+# this describes the steps/instructions that we want our code to do.
+
+def pbj():
+    print(' step 1. get 2 slices of bread')
+    print(' step 2. spread [eanutbutter on 1 slice of bread ')
+    print(' step 3. spread jellu/jam on the other slice')
+    print(' step 4. put slices together with spreads facing each other')
+    print(' step 5. sandwhich is done, enjoy :)')
+
+# to actually run and execute a function you need to write it's name
+# this is called a function call, or function invocation
+
+pbj()
+
+math()
+
+# create a function for calculatig numbers
+def calculate():
+    a = input('please enter a nuber')
+    b = input('please enter another number')
+
+    calculate()
+    
