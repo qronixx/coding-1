93b139e9cd9e2fb061296333eca151302bc15add
qronixx <tthompson-short284@boyslatin.net>
2025-09-23 12:48:14 +0000

    quiz1 1

---
+++ quiz 1.py
@@ -0,0 +1,74 @@
+# Create a new document called quiz1.py copy/ paste the following questions
+# on your document and answer the following questions.
+
+# quiz rules:
+# - There is no talking during the quiz
+# - You may only use your classnotes and w3schools.com for reference
+# - If you have a question about a quiz question, please raise your hand
+# - Once finished, submit your code to your repository using the source control 
+# button. Your commit should be "completed quiz 1."
+"For all written responses, your answer should be formatted as a string"
+
+# 1. Name and describe three (3) built-in functions in python. 
+# Please write your responses in complete sentences.
+"For your written responses, your answer should be formatted as a string"
+
+# 1 - input() is a built-in function  that allows users to type data intoa a program and assign it to a variable
+
+# 2. What data type will the following snippet of code output?
+print(30/ 232)
+
+# interger data type
+
+# 3. Identify and name the operator family each of the following
+# symbols belongs to.
+
+# and - logical operator family
+# == - comparison operator family
+# > - comparison operator family
+
+# 4. Explain the difference between the = operator and the == operator.
+# Please write your response in complete sentences.
+
+# The difference between the two is that the single equal sign is used to assign values to variables. WHereas, the double equal signs meaning its comparing if the values are the same
+
+# 5. Write code that takes a user’s input (as a string), 
+# casts it to a float, and prints the result multiplied by 2.
+
+value = input('give me a decimal to be multiplie by 2')
+print(2 * float(value))
+
+# 6. What is the difference between a parameter and an argument?
+# Please write your response in complete sentences.
+
+# A parameter is the variable listed inside the parentheses in the function definition. An argument is the value that are sent to the function when it is called.
+
+# 7. What is the difference between a function definition 
+# and a function invocation? # Please write your response in 
+# complete sentences. 
+
+# Function defintion is how we give or functions its value or purpose you could say and an invocation is us calling the fucntion so it can work.
+
+# 8. Why are functions useful in programming? Provide at least two reasons 
+# and write your reasons in in complete sentences.
+
+# Functions are important because it's how we prgram. Another reason it is important is because that's how we get our progamming/coding to show.
+
+# 9. Write a code block that uses the appropriate operator for each scenario
+x = 15
+y = 20.
+# x is greater than y
+# x and 15 are both the same
+# x and y are not the same`
+
+print(x > y)
+print(x == y)
+print(not(x == y))
+
+# 10. Create a function that will take in two values. Your function should
+# take in 1 value as a parameter, and the other value should be passed in by the
+# user through the terminal. Your function should compare if the number passed in by
+# the user is greater than the number passed in via the parameter and should print out
+# the appropriat response of true or false.
+
+def (num)
