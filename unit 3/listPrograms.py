a298b67995cf9e1b6514dff706e851ebf175987c
qronixx <tthompson-short284@boyslatin.net>
2025-10-14 12:46:44 +0000

    10/14/2025

---
+++ unit 3/listPrograms.py
@@ -0,0 +1,7 @@
+food = ['ribTips', 'Rice', 'Cabbage', 'oreos']
+food.sort()
+print(food[0]) # first item in list
+print(food[1]) # last item in list
+
+# replace the second list item with a new food, then print
+
