cbd153446a7f081ec4f524d95cbc3afe0195dc54
qronixx <tthompson-short284@boyslatin.net>
2025-10-22 12:48:28 +0000

    10/22/25

---
+++ unit 3/whileLoops.py
@@ -0,0 +1,66 @@
+# while loop - a while loop is a type of coonstruct where code
+# instructions will keep on running so long as a condition is TRUE(boolean)
+
+# NOTE - in order to stop a loop (or any program) from running in your terminal, click ctrl + c at the same time
+# while loop syntax
+
+def ageVerify():
+    videoGameAge = 17
+    purchaserAge = int(input('How old are you? '))
+
+    while videoGameAge > purchaserAge: # this is a true statement
+    #(17 > 15)
+        print('You are not old enough to buy this game')
+    else:
+        print('Thank you! Please enjoy your game.')
+
+
+#savedpw = 'mirfriedrice'
+#userpw = input('Please enter your pasword: ')
+#attempt = 0
+#while savedpw != userpw:
+ #   print('Incorrect password. Please try again.')
+ #   attempt +=1
+ #   userpw = input('Please type your password again: ')
+  #  if attempt ==2:
+  #      print('You have made too many failed attempts0. Please reset your password before trying again in a few mminutes')
+   #     break
+ #   else:
+  #      print('Welcome back to your account.')
+
+# create a cpuntdown timer using a while loop
+# your timer should start at 30 and count down to 0 by 1
+
+# activity 1
+
+def listLoop():
+    numbers = []
+    userInput = input('Please type in a number: ')
+    while userInput != 'quit':
+        newNumber =int(userInput)
+        numbers.append(newNumber)
+        print(numbers)
+        userInput = input('Please type in a number: ')
+    else:
+        print('Loop has stopped')
+    
+# istLoop()
+
+# activirt 2
+
+def addLoop():
+    numbers = []
+    userInput = input('Please enter a number: ')
+    while userInput != 'quit':
+        newNumber = int(userInput)
+        numbers.append(newNumber)
+        print(numbers)
+        userInput = input('Please enter a number: ')
+        x = sum(numbers)
+        print(x)
+    else:
+        print('Loop has ended.')
+
+    
+
+addLoop()
